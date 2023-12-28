0b5604fd4820ba36c4f62a756163613b195f32c7
thomassweet <thomas.sweet3@gmail.com>
2023-12-28 13:39:33 -0800

    Clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,13 +13,12 @@ const GameCard = ({game}:Props) => {
   return (
     <Card>
         <Image src={getCroppedImageUrl(game.background_image)} />
-        <CardBody>
-            <Heading size="lg">{game.name}</Heading>
-            <HStack justifyContent={'space-between'}>
+        <CardBody>          
+            <HStack justifyContent={'space-between'} marginBottom={3}>
                 <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
                 <CriticScore score={game.metacritic} />
             </HStack>
-            
+            <Heading size="lg">{game.name}</Heading>
         </CardBody>
     </Card>
   )
